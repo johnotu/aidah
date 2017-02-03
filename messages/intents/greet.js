7580783cafa9597a6455bd22fd2d4b4bb86cfa25
johnotu <johnne01@gmail.com>
2017-02-03 03:37:59 +0000

    Added Greet Intent

---
+++ messages/intents/greet.js
@@ -0,0 +1,12 @@
+function random(array){
+	return array[Math.floor(Math.random()*array.length)];
+}
+
+function greetings(name){
+	var answers = ['Hello ', 'Yo ;) ', 'Hey, nice to see you ', 'Hi ', 'Welcome '];
+	return (random(answers) + name);
+}
+
+module.exports = greetings
+
+//console.log(greetings('Kanye'));
